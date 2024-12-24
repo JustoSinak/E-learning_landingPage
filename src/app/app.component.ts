0d0f39c64e0bad991f48132d6936bf7b847f1cae
JustoSinak <tcheumanisinakjusto@gmail.com>
2024-12-24 17:14:14 +0100

    Update src/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { PartnersComponent } from './partners/partners.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, FeaturesComponent,
+     PricingComponent, PartnersComponent, CoursesComponent, 
+     TestimonialsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 
